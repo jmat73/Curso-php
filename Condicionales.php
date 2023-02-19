83f847e60f9dc5760f6e2689ba608e088868e244
jmat <jmat@hotmail.es>
2023-02-19 15:50:24 +0100

    Condicionales

---
+++ Condicionales.php
@@ -0,0 +1,32 @@
+<style>
+.no_validado{
+  font-size:18px;
+  color:#F00;
+  font-weight:bold;
+}
+ 
+.validado{
+  font-size:18px;
+  color:#0C3;
+  font-weight:bold;
+}
+
+</style>
+
+<?php
+if (isset ($_POST["enviando"])){
+    
+    $edad=$_POST["edad_usuario"];
+    if ($edad <=17) {
+
+        echo "Eres menor de Edad";
+    }
+
+
+
+}
+
+   
+
+
+?>
