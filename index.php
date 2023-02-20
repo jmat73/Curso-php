9a3a0513f6d2b16ebed391f6b51ac2077c0d2992
jmat <jmat@hotmail.es>
2023-02-20 10:49:06 +0100

    Versión Def

--- index.php
+++ index.php
@@ -144,4 +144,38 @@ if ($variable1 < $variable2){
  echo "vas por la línea que has visto y estas en el archivo:" . __FILE__ . "<br>";
 
  /* Paara los acentos & + vocal_con_acento + acute;+resto_palabra---> Cuídate = "Cu&íacute;date"; */
+ 
+ /* OPERADORES Y SU PRECEDENCIA **** https://www.php.net/manual/es/language.operators.precedence.php ***
+ no asociativo	clone new	clone and new
+izquierda	[	array()
+derecha	**	aritmética
+derecha	++ -- ~ (int) (float) (string) (array) (object) (bool) @	tipos e incremento/decremento
+no asociativo	instanceof	tipos
+derecha	!	lógico
+izquierda	* / %	aritmética
+izquierda	+ - .	aritmética y string
+izquierda	<< >>	bit a bit
+no asociativo	< <= > >=	comparación
+no asociativo	== != === !== <> <=>	comparación
+izquierda	&	bit a bit y referencias
+izquierda	^	bit a bit
+izquierda	|	bit a bit
+izquierda	&&	lógico
+izquierda	||	lógico
+derecha	??	comparación
+izquierda	? :	ternario
+derecha	= += -= *= **= /= .= %= &= |= ^= <<= >>=	asignación
+izquierda	and	lógico
+izquierda	xor	lógico
+izquierda	or	lógico
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  ?>
